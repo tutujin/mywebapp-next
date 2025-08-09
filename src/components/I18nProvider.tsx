d81fa97e44f533d3e4580611879b1b47dcb793d9
tutujin <pr8152262@gmail.com>
2025-08-09 21:35:49 +0800

    feat: Configure i18n and add provider

---
+++ src/components/I18nProvider.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import React from 'react';
+import { I18nextProvider } from 'react-i18next';
+import i18n from '@/i18n';
+
+interface I18nProviderProps {
+  children: React.ReactNode;
+}
+
+const I18nProvider: React.FC<I18nProviderProps> = ({ children }) => {
+  return (
+    <I18nextProvider i18n={i18n}>
+      {children}
+    </I18nextProvider>
+  );
+};
+
+export default I18nProvider;
