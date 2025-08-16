331df2db022e97d5d0664a196c189897b7e88ec6
tutujin <pr8152262@gmail.com>
2025-08-16 19:01:54 +0800

    "fix/i18n-provider-props"

--- src/components/I18nProvider.tsx
+++ src/components/I18nProvider.tsx
@@ -2,13 +2,14 @@
 
 import React from 'react';
 import { I18nextProvider } from 'react-i18next';
-import i18n from '@/i18n';
+import type { i18n as I18nInstanceType } from 'i18next';
 
 interface I18nProviderProps {
   children: React.ReactNode;
+  i18n: I18nInstanceType;
 }
 
-const I18nProvider: React.FC<I18nProviderProps> = ({ children }) => {
+const I18nProvider: React.FC<I18nProviderProps> = ({ children, i18n }) => {
   return (
     <I18nextProvider i18n={i18n}>
       {children}
